d93fe26b9eb700941ccc84ceaabae772935929ba
Ham-myeongeun <hamyeongeun@gmail.com>
2026-06-29 22:08:57 +0900

    Fix Android Gradle block structure

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -3,7 +3,6 @@ import java.util.Properties
 plugins {
     id("com.android.application")
     id("kotlin-android")
-    // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
 }
 
@@ -30,8 +29,6 @@ android {
 
     defaultConfig {
         applicationId = "com.harushim.haru"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
@@ -39,27 +36,26 @@ android {
     }
 
     signingConfigs {
-    if (keystorePropertiesFile.exists()) {
-        create("release") {
-            keyAlias = keystoreProperties["keyAlias"] as String
-            keyPassword = keystoreProperties["keyPassword"] as String
-            storeFile = rootProject.file(keystoreProperties["storeFile"] as String)
-            storePassword = keystoreProperties["storePassword"] as String
+        if (keystorePropertiesFile.exists()) {
+            create("release") {
+                keyAlias = keystoreProperties["keyAlias"] as String
+                keyPassword = keystoreProperties["keyPassword"] as String
+                storeFile = rootProject.file(keystoreProperties["storeFile"] as String)
+                storePassword = keystoreProperties["storePassword"] as String
+            }
+        }
+    }
+
+    buildTypes {
+        release {
+            signingConfig = if (keystorePropertiesFile.exists()) {
+                signingConfigs.getByName("release")
+            } else {
+                signingConfigs.getByName("debug")
+            }
         }
     }
 }
-
-buildTypes {
-    release {
-        signingConfig = if (keystorePropertiesFile.exists()) {
-            signingConfigs.getByName("release")
-        } else {
-            signingConfigs.getByName("debug")
-        }
-    }
-}
-    
-
 
 flutter {
     source = "../.."
